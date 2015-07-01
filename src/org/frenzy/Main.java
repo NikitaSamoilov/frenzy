ff0a828d6b50956bc173339a0fa0e9b6a13853ca
NikitaSamoilov <nswork1987@gmail.com>
2015-07-01 12:57:18 +0300

    Remove commented code at main

--- src/org/frenzy/Main.java
+++ src/org/frenzy/Main.java
@@ -16,43 +16,4 @@ public class Main {
         GuiWindow guiWindow = GuiWindow.buildWindow(GuiWindowType.SIMPLE, params);
         guiWindow.setVisible(true);
     }
-
-    @Deprecated
-    private static void OldProcessing() {
-//        Scanner scan = new Scanner(System.in);
-//        Scanner scanString = new Scanner(System.in);
-//        String input = null;
-//
-//
-//        System.out.println("Would you like to choose departments?");
-//        input = scan.next();
-//
-//        switch (input) {
-//            case "n":
-//                Helper.initialisation();
-//                break;
-//            case "y":
-//                System.out.println("Which departments would you choose? (By small first letter)");
-//                input = scanString.nextLine();
-//                Helper.initialisation(input);
-//                break;
-//            default:
-//                System.out.println("Wrong letter");
-//                break;
-//        }
-//
-//        Helper.setTasksForDepartment();
-//        Helper.start();
-//
-//        System.out.println("Would you like to see common flow? (y/n)");
-//        input = scan.next();
-//
-//        switch (input) {
-//            case "y":
-//                System.out.println(Helper.getFlow());
-//                break;
-//            default:
-//                break;
-//        }
-    }
 }
