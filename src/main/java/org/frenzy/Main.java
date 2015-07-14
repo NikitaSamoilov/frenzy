8e685cea7ea9569b82e0ba83a38a9f89e91414b8
NikitaSamoilov <nswork1987@gmail.com>
2015-07-14 21:51:15 +0300

    Add simple Log4j configuration and old-style Spring beans configuration. Trying to start up the context.

--- src/main/java/org/frenzy/Main.java
+++ src/main/java/org/frenzy/Main.java
@@ -1,21 +1,39 @@
 package org.frenzy;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.frenzy.core.Department;
 import org.frenzy.core.Manufacture;
 import org.frenzy.gui.GuiWindow;
 import org.frenzy.gui.GuiWindowParams;
 import org.frenzy.gui.GuiWindowType;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
     private static final int WINDOW_WIDTH = 500;
     private static final int WINDOW_HEIGHT = 400;
 
+    private static ApplicationContext applicationContext;
+
     public static void main(String[] args) {
+        initApplicationContext();
         Manufacture manufacture = new Manufacture();
         manufacture.addDepartment(new Department("Test Department"));
         GuiWindowParams params = new GuiWindowParams(WINDOW_WIDTH, WINDOW_HEIGHT, manufacture);
         GuiWindow guiWindow = GuiWindow.buildWindow(GuiWindowType.SIMPLE, params);
         guiWindow.setVisible(true);
+    }
+
+    private static void initApplicationContext() {
+        try {
+            applicationContext = new ClassPathXmlApplicationContext("FrenzyBeans.xml");
+            LOGGER.info("Spring context start successfully");
+        }
+        catch (Exception ex) {
+            LOGGER.error(ex.getStackTrace(), ex);
+        }
     }
 }
